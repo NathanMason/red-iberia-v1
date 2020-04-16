387756ff99881f92e5cf5ddf5e5e84d24349a516
Nathan Mason <nathanmason1984@hotmail.com>
2020-04-16 16:49:33 +1000

    got marklers

--- app/ng/controllers/mainController.js
+++ app/ng/controllers/mainController.js
@@ -1,34 +1,81 @@
 (function() {
 
-  angular.module('redIberia')
+      angular.module('redIberia')
 
-    .controller('mainController', ['$scope', '$rootScope', 'SocketFactory',
-      function($scope, $rootScope, SocketFactory) {
+            .controller('mainController', ['$scope', '$rootScope', 'SocketFactory', '$timeout',
+                  function($scope, $rootScope, SocketFactory, $timeout) {
 
-          SocketFactory.launchSocket( function(socket){
-              var collection = [];
+                        SocketFactory.launchSocket(function(socket) {
+                              var collection = [];
+                              $scope.markers = [];
+                              socket.onMessage(function(message) {
+                                    collection.push(JSON.parse(message.data));
+                                    var data = JSON.parse(message.data);
+                                    var geojson = {
+                                          type: 'FeatureCollection',
+                                          features: []
+                                    }
 
-              socket.onMessage(function(message) {
-                    collection.push(JSON.parse(message.data));
-                    var data = JSON.parse(message.data);
+                                    angular.forEach(data.units, function(unit) {
+                                          if (unit.action = "C") {
+                                                console.log(unit);
+                                                var data = {
+                                                      type: 'Feature',
+                                                      geometry: {
+                                                            type: 'Point',
+                                                            coordinates: [unit.lon, unit.lat]
+                                                      },
+                                                      properties: {
+                                                            icon: {
+                                                                  iconUrl: '../../img/blue-f18.png'
+                                                            }
+                                                      },
+                                                      data: {
+                                                            unit
+                                                      }
+                                                }
+                                                geojson.features.push(data)
+                                          }
 
-                    // create markers
-                    angular.forEach(data.units, function(unit) {
-                        console.log(unit);
-                    })
+                                    })
+                                    // remove all markers from map
+                                    $(".marker").remove();
 
-              });
+                                    // add new markers to map
+                                    geojson.features.forEach(function(marker) {
+                                          // create a DOM element for the marker
+                                          var el = document.createElement('div');
+                                          el.className = 'marker';
+                                          el.style.backgroundImage = 'url(img/blue-c130.png)';
+                                          el.style.width = '48px';
+                                          el.style.backgroundSize = 'contain';
+                                          el.style.height = '48px';
+                                          el.addEventListener('click', function() {
+                                                window.alert(JSON.stringify(marker.data));
+                                          });
 
-              socket.onOpen(function(message) {
-                    console.log(message);
-              });
-          });
+                                          // add marker to map
+                                          new mapboxgl.Marker(el)
+                                                .setLngLat(marker.geometry.coordinates)
+                                                .addTo($rootScope.map);
+                                    });
 
-          $scope.map = {
-              zoom: 8,
-              center: [ 42, 42 ]
-          };
+                              });
 
-      }
-    ]);
+                              socket.onOpen(function(message) {
+                                    console.log(message);
+                              });
+                        });
+
+                        mapboxgl.accessToken = 'pk.eyJ1IjoiYm9vemVyIiwiYSI6ImNrOHpidzU3bzA0eGMza29sdTJ6cmdmcXMifQ.iNvCN8OHOmQr95a_OkNLUQ';
+                        $rootScope.map = new mapboxgl.Map({
+                              container: 'map',
+                              center: [42, 42],
+                              zoom: 7,
+                              style: 'mapbox://styles/boozer/ck8zc1c2t08u51iqv1dwjp2dr'
+                        });
+
+
+                  }
+            ]);
 }());
