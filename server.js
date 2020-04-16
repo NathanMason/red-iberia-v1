0e9aeac16956535c62f50790ed84c4dd5637cc8c
Nathan Mason <nathanmason1984@hotmail.com>
2020-04-16 13:59:46 +1000

    Update server.js

--- server.js
+++ server.js
@@ -27,8 +27,6 @@ app.use('/', express.static(__dirname + '/app'));
 app.use('server', express.static(__dirname + '/server'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
-
-
 app.set('views', __dirname + '/');
 
 //////////////////////////////////////////////////////////////////////////
