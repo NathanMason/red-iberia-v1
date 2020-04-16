ac8d7c2f3a87ba54781c3fc49c650ddf07697924
Nathan Mason <nathanmason1984@hotmail.com>
2020-04-16 14:09:17 +1000

    mapBox base implemented

--- app/app.js
+++ app/app.js
@@ -1,13 +1,15 @@
 // 'ngRaven',
 (function() {
-      angular.module('redIberia', ['ngRoute', 'ngWebSocket'])
+      angular.module('redIberia', ['ngRoute', 'ngWebSocket', 'angularMapbox'])
 
             // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
             // Routes
             // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-            .config(function($routeProvider, $locationProvider, $httpProvider) {
+            .config(function($routeProvider, $locationProvider, $httpProvider, angularMapboxConfigProvider) {
 
-
+                angularMapboxConfigProvider.config({
+                                accessToken: 'pk.eyJ1IjoiYm9vemVyIiwiYSI6ImNrOHpidzU3bzA0eGMza29sdTJ6cmdmcXMifQ.iNvCN8OHOmQr95a_OkNLUQ'
+                            });
                   $locationProvider.html5Mode(true);
                   $locationProvider.hashPrefix('');
                   $routeProvider
