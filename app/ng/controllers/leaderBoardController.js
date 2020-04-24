f5ae3614590a1aa90fd252f490a7f45b813bcf85
Nathan Mason <nathanmason1984@hotmail.com>
2020-04-24 16:04:37 +1000

    table

--- app/ng/controllers/leaderBoardController.js
+++ app/ng/controllers/leaderBoardController.js
@@ -2,8 +2,8 @@
 
       angular.module('redIberia')
 
-            .controller('leaderBoardController', ['$scope', '$rootScope', '$http', '$timeout', '$route',
-                  function($scope, $rootScope, $http, $timeout,$route) {
+            .controller('leaderBoardController', ['$scope', '$rootScope', '$http', '$timeout', '$route', 'LeaderBoardFunctions',
+                  function($scope, $rootScope, $http, $timeout,$route, LeaderBoardFunctions) {
 
                       $rootScope.pilotStatistics = [];
                       $rootScope.$route = $route;
@@ -14,26 +14,23 @@
                         data: {},
                         //on successful data reception, we can now show the data and make it all functional
                         success: function(data) {
-
                           if (data === false) {
                             console.log('ERROR: Server could not return a valid database object.');
                             return;
                         } else {
-                            console.log(data);
-                            $timeout(function(){
-                                $rootScope.pilotStatistics = data[0].stats;
-                            }, 100);
+                            LeaderBoardFunctions.sortPilotStats(data[0].stats, function(r){
+
+                                $timeout(function(){
+                                    $rootScope.pilotStatistics = r;
+                                }, 100);
+
+
+                            })
+
                         }
                         }, //end success
                         error: function(err) { console.log(err) }
                       }); //end ajax call
-
-
-                      $scope.totalDisplayed = 10;
-
-                      $scope.loadMore = function () {
-                        $scope.totalDisplayed += 10;
-                      };
 
                   }
             ]);
---
+++ app/ng/services/leaderBoardFunctions.js
@@ -0,0 +1,84 @@
+(function() {
+      angular.module("redIberia").factory("LeaderBoardFunctions", function($rootScope, $q) {
+
+            var LeaderBoardFunctions = {};
+
+
+            LeaderBoardFunctions.sortPilotStats = function(obj, cb) {
+                    // create the pilot object
+                    var stats = [];
+                    var promises = [];
+                    // calculate leaderboard positions
+
+                    angular.forEach(obj, function(pilot){
+                        var q = $q.defer();
+                        promises.push(q.promise);
+
+                        var currentPilot = {
+                            callSign: pilot.name,
+                            pvp: { kills: pilot.PvP.kills, losses: pilot.PvP.losses },
+                            friendlyHits: {kills: pilot.friendlyKills, hits: pilot.friendlyHits },
+                            kills: {buildings: pilot.kills.Buildings, groundUnits: pilot.kills[ 'Ground Units' ], rotorUnits: pilot.kills.Helicopters },
+                            losses: {crash: pilot.losses.crash, eject: pilot.losses.eject, pilotDeath: pilot.losses.pilotDeath}
+                            }
+
+                            currentPilot.ranking = LeaderBoardFunctions.calcRank(currentPilot);
+                            currentPilot.totalDeaths = LeaderBoardFunctions.getTotalDeaths(currentPilot);
+                            currentPilot.totalKills = LeaderBoardFunctions.getTotalKills(currentPilot);
+                            currentPilot.totalIncidents = LeaderBoardFunctions.getTotalIncidents(currentPilot);
+                        stats.push(currentPilot)
+                        q.resolve();
+
+                    });
+
+                    $q.all(promises).then(function() {
+                        cb(stats)
+                    })
+            }
+
+             LeaderBoardFunctions.calcRank = function(i) {
+
+                return rank = LeaderBoardFunctions.getTotalKills(i) - LeaderBoardFunctions.getTotalDeaths(i) - LeaderBoardFunctions.getTotalIncidents(i);
+
+             }
+
+             LeaderBoardFunctions.getTotalKills = function(i) {
+
+                return totalKills = i.pvp.kills + i.kills.buildings.total + i.kills.groundUnits.total + i.kills.rotorUnits.total;
+             }
+
+             LeaderBoardFunctions.getTotalDeaths = function(i) {
+                return totalDeaths = i.pvp.losses + i.losses.crash + i.losses.eject + i.losses.pilotDeath;
+             }
+
+             LeaderBoardFunctions.getTotalIncidents = function(i) {
+                 var killCount = 0;
+                 var hitCount = 0;
+                 if (i.friendlyHits.kills == null) {
+                    killCount = 0
+                } else {
+                    angular.forEach(i.friendlyHits.kills, function(value, key) {
+                      killCount++
+                    });
+                }
+                 if (i.friendlyHits.hits == null) {
+                    hitCount = 0;
+                } else {
+                    angular.forEach(i.friendlyHits.hits, function(value, key) {
+                      hitCount++
+                    });
+                }
+                console.log(hitCount);
+                console.log(killCount);
+                return totalIncidents = killCount + hitCount;
+             }
+
+            return LeaderBoardFunctions;
+
+
+
+
+
+      });
+
+})();
